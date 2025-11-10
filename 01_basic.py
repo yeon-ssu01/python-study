5e13cefa55650e0cb41a533a198c3cb6be5452d7
yeon-ssu01 <dustjd23@gmail.com>
2025-11-10 20:45:16 +0900

    Add Python basic practice

---
+++ 01_basic.py
@@ -0,0 +1,39 @@
+print("Hello Pyton")
+print(10+20)
+
+name = "Yeon"
+age = 34
+running_speed = 4.5
+print(name, age, running_speed)
+
+text = "run"
+count = 42
+pace = 4.2
+good = True
+print(type(text), type(count), type(pace), type(pace), type(good))
+
+nickname = input("닉네임 입력")
+print("환영합니다,", nickname)
+
+a = int(input("숫자 입력: "))
+b = int(input("숫자 입력: "))
+print("합:", a + b)
+
+print(10+3)
+print(10-3)
+print(10*3)
+print(10/3)
+print(10//3)
+print(10%3)
+print(10**2)
+
+a = int(input("첫 숫자: "))
+10
+b = int(input("두번째 숫자: "))
+5
+print("합:", a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+
+Add Python basic practice
